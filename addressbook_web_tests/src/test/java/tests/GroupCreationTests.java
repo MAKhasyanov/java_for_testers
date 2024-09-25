7c1df4cae54fd2f8a81da85404e74279e2748a6a
MAKhasyanov <makhasyanov@gmail.com>
2024-09-26 01:41:59 +0300

    Лекция 5.5.

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -8,17 +8,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
     public static List<GroupData> groupProvider() throws IOException {
-        var result=new ArrayList<GroupData>();
+        var result = new ArrayList<GroupData>();
 //        for (var name:List.of("","group name")){
 //            for(var header:List.of("","group header")){
 //                for(var footer:List.of("","group footer")){
@@ -32,42 +36,55 @@ public class GroupCreationTests extends TestBase{
 //                    .withHeader(CommonFunctions.randomString(i*10))
 //                    .withFooter(CommonFunctions.randomString(i*10)));
 //        }
-        ObjectMapper mapper=new ObjectMapper();
-        var value=mapper.readValue(new File("groups.json"),new TypeReference<List<GroupData>>(){});
-        result.addAll(value);
-        return result;
+
+//        var json = "";
+//        try (var reader = new FileReader("groups.json");
+//             var breader = new BufferedReader(reader)) {
+//            var line = breader.readLine();
+//            while (line != null) {
+//                json=json+line;
+//                line= breader.readLine();
+//            }
+//            }
+            var json = Files.readString(Paths.get("groups.json"));
+            ObjectMapper mapper = new ObjectMapper();
+
+            var value = mapper.readValue(json, new TypeReference<List<GroupData>>() {
+            });
+            result.addAll(value);
+            return result;
+        }
+
+        @ParameterizedTest
+        @MethodSource("groupProvider")
+        public void canCreateMultipleGroups (GroupData group){
+            var oldGroups = app.groups().getList();
+            app.groups().CreateGroup(group);
+            var newGroups = app.groups().getList();
+            Comparator<GroupData> compareById = (o1, o2) -> {
+                return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+            };
+            newGroups.sort(compareById);
+
+            var expectedList = new ArrayList<>(oldGroups);
+            expectedList.add(group.withId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
+            expectedList.sort(compareById);
+            Assertions.assertEquals(newGroups, expectedList);
+
+        }
+
+
+        public static List<GroupData> negativeGroupProvider () {
+            var result = new ArrayList<GroupData>(List.of(
+                    new GroupData("", "group name'", "", "")));
+            return result;
+        }
+        @ParameterizedTest
+        @MethodSource("negativeGroupProvider")
+        public void canNotCreateGroups (GroupData group){
+            var oldGroups = app.groups().getList();
+            app.groups().CreateGroup(group);
+            var newGroups = app.groups().getList();
+            Assertions.assertEquals(newGroups, oldGroups);
+        }
     }
-
-    @ParameterizedTest
-    @MethodSource("groupProvider")
-    public void canCreateMultipleGroups(GroupData group) {
-        var oldGroups =app.groups().getList();
-        app.groups().CreateGroup(group);
-        var newGroups =app.groups().getList();
-        Comparator<GroupData> compareById = (o1, o2) -> {
-            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
-        };
-        newGroups.sort(compareById);
-
-        var expectedList=new ArrayList<>(oldGroups);
-        expectedList.add(group.withId(newGroups.get(newGroups.size()-1).id()).withHeader("").withFooter(""));
-        expectedList.sort(compareById);
-        Assertions.assertEquals(newGroups,expectedList);
-
-    }
-
-
-    public static List<GroupData> negativeGroupProvider() {
-        var result=new ArrayList<GroupData>(List.of(
-                new GroupData("", "group name'","","")));
-        return result;
-    }
-    @ParameterizedTest
-    @MethodSource("negativeGroupProvider")
-    public void canNotCreateGroups(GroupData group) {
-        var oldGroups =app.groups().getList();
-        app.groups().CreateGroup(group);
-        var newGroups =app.groups().getList();
-        Assertions.assertEquals(newGroups,oldGroups);
-    }
-}
