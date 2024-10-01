19b854b15b34f3181d2229070688e7e35afa7055
MAKhasyanov <makhasyanov@gmail.com>
2024-10-02 01:20:42 +0300

    Лекция 7.1. Функциональный стиль программирования

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -5,13 +5,23 @@ import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
 import ru.stqa.geometry.figures.Triangle;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Geometry {
     public static void main(String[] args) {
-      //  Square.printSquareArea(new Square(7.0));
-      //  Square.printSquareArea(new Square(5.0));
-        Square.printSquareArea(new Square(3.0));
+       var squares= List.of(new Square(7.),new Square(5.),new Square(3.));
+//        for (Square square:squares){
+//            Square.printSquareArea(square);
+//        }
 
-       // Rectangle.printRectangleArea(new Rectangle(3.,5.));
+ //       Consumer<Square> print= Square::printSquareArea;
+        squares.forEach(Square::printSquareArea);
+//        Square.printSquareArea(new Square(7.0));
+//        Square.printSquareArea(new Square(5.0));
+//        Square.printSquareArea(new Square(3.0));
+
+        Rectangle.printRectangleArea(new Rectangle(3.,5.));
         Rectangle.printRectangleArea(new Rectangle(7.0,9.0));
 
         Triangle.printTriangleArea(new Triangle(5.,5.,5.));
